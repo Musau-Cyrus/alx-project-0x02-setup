7e15274e47126c3c192271e6d2140c87e6129983
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-06-08 20:28:24 +0300

    An update on usage of Header

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,8 +1,10 @@
 import Button from '@/components/common/Button';
+import Header from '@/components/layout/Header';
 
 const About = () => {
     return (
         <div>
+            <Header/>
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias amet natus dolorem sint! Quos a assumenda beatae non nisi quam autem, temporibus cum ipsum culpa nesciunt aliquid quas laborum ex?</h1>
             <div style={{ marginTop: '20px' }}>
                 <Button size="small" shape="rounded-sm" label="Small Rounded">Small Rounded</Button>
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,10 @@
 import Card from "@/components/common/Card";
+import Header from "@/components/layout/Header";
 
 const Home = () =>{
     return(
         <div>
-      
+        <Header/>
         <h1>Welcome to the Home Page!</h1>
         <Card title="Sample Title" content="Sample content goes here." />
         </div>
