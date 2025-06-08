0f8cbf4d9d15de24ddbcd3b9beb08f6752bc00c9
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-06-08 11:55:34 +0300

    Update the header

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: '#000', color: '#fff', padding: '1rem' }}>
+      <nav>
+        <Link href="/home" legacyBehavior>
+          <a style={{ color: '#fff', marginRight: '1rem' }}>Home</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a style={{ color: '#fff', marginRight: '1rem' }}>About</a>
+        </Link>
+        <Link href="/posts" legacyBehavior>
+          <a style={{ color: '#fff' }}>Posts</a>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
