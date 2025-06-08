ecad9a25b833bca32d791d680b7e5c6c94c3ef55
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-06-08 12:15:05 +0300

    An update to the project

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,8 @@
+const About = () =>{
+    return(
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias amet natus dolorem sint! Quos a assumenda beatae non nisi quam autem, temporibus cum ipsum culpa nesciunt aliquid quas laborum ex?</h1>
+
+    )
+}
+
+export default About;
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const Home: React.FC = () =>{
   return(
     <div>
+        <Header/>
          <p>welcome!</p>
     </div>
   )
